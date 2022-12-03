bdd046f42e37cb1908c95253bff0964cbeec51ea
Karim Bakkali <kbldn@live.co.uk>
2022-12-03 13:49:52 +0000

    code highlighting.

--- 2/a.js
+++ 2/a.js
@@ -1,8 +1,7 @@
 // https://adventofcode.com/2022/day/2
 import fs from 'fs'
-var text = fs.readFileSync('2\\input2.txt', 'utf8')
+var text = fs.readFileSync('2\\input.txt', 'utf8')
 var textByLine = text.split('\r\n').filter(Boolean)
-console.log(textByLine)
 
 let scores = {
     A: 1,
@@ -40,47 +39,52 @@ let map = {
 let score = 0
 
 // Part 1
-// for(const line of textByLine){
-//     let plays = line.split(' ');
-//         if(scores[plays[0]] === scores[plays[1]]){
-//             score += 3;
-//             score += scores[plays[1]];
-//             continue;
-//         }
-//         if(scores[plays[1]] > scores[plays[0]]){
+for(const line of textByLine){
+    let plays = line.split(' ');
+        if(scores[plays[0]] === scores[plays[1]]){
+            score += 3;
+            score += scores[plays[1]];
+            continue;
+        }
+        if(scores[plays[1]] > scores[plays[0]]){
 
-//             if(scores[plays[0]] === 1 && scores[plays[1]] === 3){
-//                 score += scores[plays[1]];
-//                 continue;
-//             }
-//             score += scores[plays[1]];
-//             score += 6;
-//             continue;
-//         }else if(scores[plays[1]] < scores[plays[0]]){
-//             if(scores[plays[1]] === 1 && scores[plays[0]] === 3){
-//                 score += scores[plays[1]];
-//                 score += 6;
-//                 continue;
-//             }
-//             score += scores[plays[1]];
-//             continue;
-//         }
-// }
-// console.log(score)
+            if(scores[plays[0]] === 1 && scores[plays[1]] === 3){
+                score += scores[plays[1]];
+                continue;
+            }
+            score += scores[plays[1]];
+            score += 6;
+            continue;
+        }else if(scores[plays[1]] < scores[plays[0]]){
+            if(scores[plays[1]] === 1 && scores[plays[0]] === 3){
+                score += scores[plays[1]];
+                score += 6;
+                continue;
+            }
+            score += scores[plays[1]];
+            continue;
+        }
+}
+console.log("Part 1", score);
+
+
+var text2 = fs.readFileSync('2\\input2.txt', 'utf8')
+var textByLine2 = text2.split('\r\n').filter(Boolean)
 
 // Part 2
-// for(const line of textByLine){
-//     let plays = line.split(' ');
-//     let weNeedTo = outcome[plays[1]];
-//     let playToMake = map[plays[0]][outcome[plays[1]]]
-//     if(weNeedTo === "Win"){
-//         score += scores[playToMake]
-//         score += 6
-//     }else if(weNeedTo === "Lose"){
-//         score += scores[playToMake]
-//     }else{
-//         score += scores[playToMake]
-//         score += 3
-//     }
-// }
-// console.log(score)
+let score2 = 0
+for(const line of textByLine2){
+    let plays = line.split(' ');
+    let weNeedTo = outcome[plays[1]];
+    let playToMake = map[plays[0]][outcome[plays[1]]]
+    if(weNeedTo === "Win"){
+        score2 += scores[playToMake]
+        score2 += 6
+    }else if(weNeedTo === "Lose"){
+        score2 += scores[playToMake]
+    }else{
+        score2 += scores[playToMake]
+        score2 += 3
+    }
+}
+console.log("Part 2", score2);
--- 3/a.js
+++ 3/a.js
@@ -10,53 +10,45 @@ for(let i = 1; i <= 26; i++){
     priority.set(String.fromCharCode(64 + i), i + 26);
 }
 
-
-
-
 // Part 1 
-// let sumOfPriorities = 0;
-// for(const line of textByLine1){
-//     let middle = Math.ceil(line.length / 2);
-//     let compartments = [];
-//     compartments.push(line.split('').splice(0, middle));
-//     compartments.push(line.split('').splice(middle, line.length));
-//     // find the difference between the two compartments
-//     let intersections = [...new Set(compartments[0].filter(x => compartments[1].includes(x)))];
-//     // sum the priority of the intersections
-//     let interSum = 0;
-//     for(const intersection of intersections){
-//         interSum += priority.get(intersection);
-//     }
-//     sumOfPriorities += interSum;    
-// }
-// console.log(sumOfPriorities);
+let sumOfPriorities = 0;
+for(const line of textByLine1){
+    let middle = Math.ceil(line.length / 2);
+    let compartments = [];
+    compartments.push(line.split('').splice(0, middle));
+    compartments.push(line.split('').splice(middle, line.length));
+    // find the difference between the two compartments
+    let intersections = [...new Set(compartments[0].filter(x => compartments[1].includes(x)))];
+    // sum the priority of the intersections
+    let interSum = 0;
+    for(const intersection of intersections){
+        interSum += priority.get(intersection);
+    }
+    sumOfPriorities += interSum;    
+}
+console.log("Part 1",sumOfPriorities);
 
 // Part 2
-// var text2 = fs.readFileSync('3\\input2.txt', 'utf8')
-// var textByLine2 = text2.split('\r\n').filter(Boolean)
-// let sumOfPriorities2 = 0;
+var text2 = fs.readFileSync('3\\input2.txt', 'utf8')
+var textByLine2 = text2.split('\r\n').filter(Boolean)
+let sumOfPriorities2 = 0;
 
-// // split into groups of 3
-// let groupsOfElves = [];
-// for(let i = 0; i < textByLine2.length; i += 3){
-//     groupsOfElves.push(textByLine2.slice(i, i + 3));
-// }
+// split into groups of 3
+let groupsOfElves = [];
+for(let i = 0; i < textByLine2.length; i += 3){
+    groupsOfElves.push(textByLine2.slice(i, i + 3));
+}
 
-// for(const group of groupsOfElves){
-//     // find the intersections of each group of elves, one at a time
-//     let intersections = [...new Set(group[0].split('').filter(x => group[1].split('').includes(x)))];
-//     intersections = [...new Set(intersections.filter(x => group[2].split('').includes(x)))];
-//     // sum the priority of the intersections
-//     let interSum = 0;
-//     for(const intersection of intersections){
-//         interSum += priority.get(intersection);
-//     }
-//     sumOfPriorities2 += interSum;
-// }
+for(const group of groupsOfElves){
+    // find the intersections of each group of elves, one at a time
+    let intersections = [...new Set(group[0].split('').filter(x => group[1].split('').includes(x)))];
+    intersections = [...new Set(intersections.filter(x => group[2].split('').includes(x)))];
+    // sum the priority of the intersections
+    let interSum = 0;
+    for(const intersection of intersections){
+        interSum += priority.get(intersection);
+    }
+    sumOfPriorities2 += interSum;
+}
 
-// console.log(sumOfPriorities2);
-
-
-
-
-
+console.log("Part 2",sumOfPriorities2);
