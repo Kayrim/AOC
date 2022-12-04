5ef1500a074092657a15daa359f0ed05f6df7519
Karim Bakkali <kbldn@live.co.uk>
2022-12-04 14:54:16 +0000

    refactoring part 1 to show both answers.

--- 1/a.js
+++ 1/a.js
@@ -7,14 +7,13 @@ let elves = new Map();
 let calories = [];
 
 while (textByLine.length > 0) {
-    let index = 0;
+    
     let line = textByLine.shift();
     if(line !== ""){
         calories.push(line);
     }else{
         let total = calories.reduce((a, b) => parseInt(a) + parseInt(b), 0);
         elves.set(total, calories);
-        index++;
         calories = [];
     }
 }
@@ -22,12 +21,13 @@ while (textByLine.length > 0) {
 // order the map by key
 let sorted = new Map([...elves.entries()].sort((a, b) => a[0] - b[0]));
 
+// Part 1 
+console.log("Part 1", [...sorted].slice(-1)[0][0]);
+
 // grap the last 3 
 let last3 = [...sorted].slice(-3);
-
-console.log(last3);
 
 // add up the keys
 let total = last3.reduce((a, b) => a + b[0], 0);
 
-console.log(total);
+console.log("Part 2", total);
