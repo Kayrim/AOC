0ad46ca4a424f5eb696e208595d4a80e56f7f4ab
Karim Bakkali <kbldn@live.co.uk>
2022-12-13 13:07:06 +0000

    Ahhh still dont get it.

--- 13/a.js
+++ 13/a.js
@@ -1,23 +1,16 @@
 // https://adventofcode.com/2022/day/13
 import fs from 'fs'
 // let text = fs.readFileSync('13\\test.txt', 'utf8').trim()
-let text = fs.readFileSync('13\\input.txt', 'utf8').trim()
-
-let packets = text.split('\r\n\r\n').map(x => x.split('\r\n'));
-
+let text = fs.readFileSync('13\\input.txt', 'utf8').trim();
+let packets = text.split('\r\n\r\n').map(x => x.split('\r\n').map(y => JSON.parse(y)));
 let goodPackets = 0;
-let index = 0;
 
 packets.forEach((packet, index) => {
     let [left, right] = packet;
-    // JSON parse the packet to make it easier to work with
-    left = JSON.parse(left);
-    right = JSON.parse(right);
-    // console.log(left, right);
 
     // compare the two packets
     let result = myCompare(left, right);
-    if(result === 1 ){
+    if(result === 1){
         goodPackets += index + 1;
     }
 });
@@ -43,24 +36,19 @@ function myCompare(a, b){
 
     // if there both arrays
     if(Array.isArray(a) && Array.isArray(b)){
-        let i = 0;
-        while(i < a.length && i < b.length){
-            let result = myCompare(a[i], b[i]);
-            if(result === 1 || result === -1){
-                return result;
-            }
-            i++;
-        }
-
-        if(i === a.length){
-            if(a.length === b.length){
-                return 0;
-            }else{
-                return 1;
+        let res;
+        for(let i = 0; i < Math.min(a.length, b.length); i++){
+            let res = myCompare(a[i], b[i]);
+            if(res !== 0){
+                return res;
             }
         }
 
-        // we must be at the end of b
-        return -1;        
+        // if we get here, we have to check the length
+        if(a.length < b.length){
+            return 1;
+        }else if(a.length >= b.length){
+            return -1;
+        }
     }
 }
